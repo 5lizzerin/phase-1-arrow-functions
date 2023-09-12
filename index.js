f44dacc363e244911e4d9e738648678345269068
Lizz <lizzcklein@gmail.com>
2023-09-12 10:53:53 -0400

    tests

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (num1, num2) => num1/num2;
+divide(2000,100);
+
+
+const square = (par1) => par1 * par1;
+square(4);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(7,8);
+
+
+
+// 1) has a function expression called divide
+// 2) divide divides 2000 by 100
+// 3) has an arrow function called square
+// 4) square arrow function takes one parameter and multiplies it times itself
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
